693024cfc6e6b95e8f2b8885ce1cab0fe774b5f2
BADDY MUZIK <101480645+BaddyM@users.noreply.github.com>
2023-11-27 22:13:02 +0300

    Second commit

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/datepicker.css">
     <link rel="stylesheet" href="css/swiper-bundle.min.css">
     <title>Test</title>
 </head>
@@ -86,14 +87,14 @@
                 </div>
                 <div class="layer-two-r1">
                     <p class="mb-0" style="font-size: 13px;">Check-in</p>
-                    <p style="font-size: 11px;"  class="mb-0 text-muted">Add Date</p>
+                    <input type="text" class="check-in" style="font-size: 11px;" placeholder="Add Date">
                 </div>
                 <div class="border-layer-two">
 
                 </div>
                 <div class="layer-two-r1">
                     <p class="mb-0" style="font-size: 13px;">Check-out</p>
-                    <p style="font-size: 11px;"  class="mb-0 text-muted">Add Date</p>
+                    <input type="text" class="check-out" style="font-size: 11px;" placeholder="Add Date">
                 </div>
                 <div class="layer-two-r1 ms-5">
                     <button class="btn explore-btn px-3 py-2 text-white">EXPLORE NOW</button>
@@ -152,8 +153,33 @@
                 <h5 class="text-center recommend-title">Recommended Destination</h5>
 
             <div>
-
+                <!-- Nav tabs -->
+                <div class="d-flex justify-content-center">
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="asia-tab" data-bs-toggle="tab" data-bs-target="#asia" type="button">Asia</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="europa-tab" data-bs-toggle="tab" data-bs-target="#europa" type="button">Europa</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="africa-tab" data-bs-toggle="tab" data-bs-target="#africa" type="button">Africa</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="america-tab" data-bs-toggle="tab" data-bs-target="#america" type="button">America</button>
+                    </li>
+                    </ul>
+                </div>
+                
+                <!-- Tab panes -->
+                <div class="tab-content">
+                  <div class="tab-pane active" id="asia" role="tabpanel" aria-labelledby="home-tab"> Asia </div>
+                  <div class="tab-pane" id="europa" role="tabpanel" aria-labelledby="profile-tab"> Europa </div>
+                  <div class="tab-pane" id="africa" role="tabpanel" aria-labelledby="messages-tab"> Africa </div>
+                  <div class="tab-pane" id="america" role="tabpanel" aria-labelledby="messages-tab"> America </div>
+                </div>
             </div><!-- Layer 4 -->
+
         </div><!-- Body content -->
 
         <footer class="mt-3">
@@ -165,8 +191,17 @@
     <!-- Scripts here -->
     <script src="js/jquery.min.js"></script>
     <script src="js/swiper-bundle.min.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
 
     <script>
+        //Initialize the date picker
+        $(".check-in, .check-out").datepicker({
+            todayHiglight: true,
+	        autoclose: true,
+            format:'dd M yy'
+        })
+
         //Swiper slider here
         const swiper = new Swiper('.swiper', {
             direction: 'horizontal',
